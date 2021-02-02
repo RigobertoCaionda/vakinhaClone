51b4dc77432d2198ba2bb67b9eca4af7b9317638
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2021-02-02 14:32:22 +0100

    In this commit i have created the menu of this project.

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+function onFocus(){
+	let searchInput = document.querySelector('.searchInput');
+		let alertDiv = document.createElement('div');
+		alertDiv.classList.add('alertDiv');
+		alertDiv.innerHTML = 'Digite ao menos 3 caracteres.';
+		searchInput.parentElement.insertBefore(alertDiv, searchInput.ElementSibling);
+		searchInput.style.outline = 'none';
+		searchInput.style.border = "4px solid #bfdeff";
+		alert == true;
+}
+function changeBorder(){
+	let remove = document.querySelector('.alertDiv');
+	remove.remove(remove);
+	let searchInput = document.querySelector('.searchInput');
+	searchInput.style.border = "1px solid #ced4da";
+	//Proximo passo, fazer com que ao clicar mais uma vez no input o divAlert desapareca
+}
